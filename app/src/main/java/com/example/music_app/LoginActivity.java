7431b952f9060ef99385302fe75b024095c74860
Hwngne <Xhung2k3@gmail.com>
2024-08-18 20:21:29 +0700

    Code log in UI

--- app/src/main/java/com/example/music_app/LoginActivity.java
+++ app/src/main/java/com/example/music_app/LoginActivity.java
@@ -1,8 +1,6 @@
 package com.example.music_app;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,22 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class SigninActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_signin);
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        Button btn = findViewById(R.id.btn_login);
-        btn.setOnClickListener(v -> {
-            Intent intent = new Intent(SigninActivity.this, LoginActivity.class);
-            startActivity(intent);
-            finish();
         });
     }
 }
