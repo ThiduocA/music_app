f4eadebef219dc38f4e4a686e7bd5baf744e9afe
Hwngne <Xhung2k3@gmail.com>
2024-08-19 20:10:59 +0700

    Code navigation and fragment

--- app/src/main/java/com/example/music_app/MainActivity.java
+++ app/src/main/java/com/example/music_app/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.music_app;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+private BottomNavigationView bottomNavigationView;
+private FrameLayout frameLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,35 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        frameLayout = findViewById(R.id.frame_layout);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.navHome) {
+                    loadFragment(new HomeFragment(), false);
+                }else if(itemId == R.id.navExplore){
+                    loadFragment(new ExploreFragment(), false);
+                }else if(itemId == R.id.navLibrary){
+                    loadFragment(new LibraryFragment(), false);
+                }
+                return true;
+            }
+        });
+        loadFragment(new HomeFragment(), true);
+    }
+
+    private void loadFragment(Fragment fragment, boolean isAppInitialized){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if(isAppInitialized){
+            fragmentTransaction.add(R.id.frame_layout, fragment);
+        }else{
+            fragmentTransaction.replace(R.id.frame_layout, fragment);
+        }
+        fragmentTransaction.commit();
     }
 }
