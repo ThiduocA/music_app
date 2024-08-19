702ff9e879137abb61264ab9879a1ffff2e4ba3a
Hwngne <Xhung2k3@gmail.com>
2024-08-19 09:32:46 +0700

    Code log in activity

--- app/src/main/java/com/example/music_app/LoginActivity.java
+++ app/src/main/java/com/example/music_app/LoginActivity.java
@@ -1,6 +1,12 @@
 package com.example.music_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +14,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.regex.Pattern;
 
+public class LoginActivity extends AppCompatActivity {
+private EditText editTextEmail, editTextPassword;
+private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,30 @@ public class LoginActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        editTextEmail = findViewById(R.id.editTextTextEmailAddress);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+        editTextEmail.setBackgroundResource(0);
+        editTextPassword.setBackgroundResource(0);
+        btn = findViewById(R.id.btn_login);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                Pattern pattern = Patterns.EMAIL_ADDRESS;
+                boolean isValidEmail = pattern.matcher(email).matches();
+                if(email.isEmpty() || password.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Email or password not null", Toast.LENGTH_LONG).show();
+                }else if(!isValidEmail){
+                    Toast.makeText(getApplicationContext(), "Invalid email", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "Login successfully", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
         });
     }
 }
